d072b9f662ead292b7d6252cf40fc1e7ad98798d
timostrating <timostrating@home.nl>
2019-02-14 23:23:12 +0100

    iterator

--- src/week3/Iterator/MyStack.java
+++ src/week3/Iterator/MyStack.java
@@ -1,10 +1,38 @@
-import java.util.Iterator;
+package week3.Iterator;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class MyStack implements Iterable<String> {
+public class MyStack<T> implements Iterable<T> {
 
-    private ArrayList<String> list = new ArrayList<>();
+    private ArrayList<T> list = new ArrayList<>();
 
-    // your code
-    
+    @Override
+    public Iterator<T> iterator() {
+       return new StackIterator();
+    }
+
+    public void push(T item) {
+        list.add(item);
+    }
+
+    public T pop() {
+        return list.remove(list.size()-1);
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+    class StackIterator implements Iterator<T> {
+        @Override
+        public boolean hasNext() {
+            return !isEmpty();
+        }
+
+        @Override
+        public T next() {
+            return pop();
+        }
+    }
 }
