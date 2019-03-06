c1123d3addf6f4b9c5aa0e629442bbb35a36f712
timostrating <timostrating@home.nl>
2019-03-06 11:15:36 +0100

    oke

---
+++ src/week4/chat/Server.java
@@ -0,0 +1,36 @@
+package week4.chat;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+
+	ServerSocket ss;
+	ArrayList<ServerConnection> connections = new ArrayList<ServerConnection>();
+	boolean shouldrun = true;
+
+	public static void main(String[] args) {
+		new Server();
+	}
+
+	public Server() {
+		try {
+			ss = new ServerSocket(3333);
+			while (shouldrun) {
+
+				Socket s = ss.accept();
+				ServerConnection sc = new ServerConnection(s, this);
+				sc.start();
+				connections.add(sc);
+
+			}
+
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+
+	}
+}
